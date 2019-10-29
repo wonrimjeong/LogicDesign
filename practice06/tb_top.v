422cd9b308ae176b1c351e960e04531593448b62
wonrimjeong <43230423+wonrimjeong@users.noreply.github.com>
2019-10-29 21:08:01 +0900

    top

---
+++ practice06/tb_top.v
@@ -0,0 +1,28 @@
+`timescale	1ns/1ns
+module	tb_top;
+
+parameter	tCK = 1000/50;
+
+wire 	[5:0] 	o_seg_enb 	;
+wire		o_seg_dp	; 
+wire	[6:0]	o_seg 		;
+
+reg		clk		;
+reg		rst_n		;
+
+initial			clk = 1'b0;
+always	#(tCK/2)	clk = ~clk;
+	
+top_nco_cnt_disp	dut0(	.o_seg_enb	(o_seg_enb),
+				.o_seg_dp	(o_seg_dp),
+				.o_seg		(o_seg),
+				.clk		(clk),
+				.rst_n		(rst_n)	);
+
+initial begin
+	#(0*tCK) rst_n = 1'b0; 
+	#(1*tCK) rst_n = 1'b1; 
+	#(100000000*tCK) $finish;
+end
+
+endmodule
